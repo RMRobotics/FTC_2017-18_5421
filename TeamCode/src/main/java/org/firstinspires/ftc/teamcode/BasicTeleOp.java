da14ecd105786cfc879b0782252d76ff493a5c42
DanWilliams24 <01danielwilliams@gmail.com>
2017-10-31 19:15:00 -0400

    Basic Tele-Op Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.ArrayList;
@@ -17,48 +18,92 @@ import java.util.*;
 public class BasicTeleOp extends OpMode {
 
     private ElapsedTime timer;
-    private DcMotor wheelBR;
-    private DcMotor wheelBL;
-    private DcMotor wheelFL;
-    private DcMotor wheelFR;
+    private DcMotor BR;
+    private DcMotor BL;
+    private DcMotor FL;
+    private DcMotor FR;
     private DcMotor glyphGrabber;
     private DcMotor liftR;
     private DcMotor liftL;
     private DcMotor relicGrabber;
+    private Servo relicHand;
+    private Servo relicArm;
+    //private Servo;
 
     public void init(){
-        wheelBR = hardwareMap.dcMotor.get("wheelBR");
-        wheelBL = hardwareMap.dcMotor.get("wheelBL");
-        wheelFL = hardwareMap.dcMotor.get("wheelFL");
-        wheelFR = hardwareMap.dcMotor.get("wheelFR");
+        BR = hardwareMap.dcMotor.get("wheelBR");
+        BL = hardwareMap.dcMotor.get("wheelBL");
+        FL = hardwareMap.dcMotor.get("wheelFL");
+        FR = hardwareMap.dcMotor.get("wheelFR");
+        relicHand = hardwareMap.servo.get("relicHand");
+        relicArm = hardwareMap.servo.get("relicArm");
         liftL = hardwareMap.dcMotor.get("liftL");
         liftR = hardwareMap.dcMotor.get("liftR");
         glyphGrabber = hardwareMap.dcMotor.get("glyphGrabber");
         relicGrabber = hardwareMap.dcMotor.get("relicGrabber");
-        wheelBR.setDirection(DcMotor.Direction.FORWARD);
-        wheelBL.setDirection(DcMotor.Direction.FORWARD);
-        wheelFL.setDirection(DcMotor.Direction.FORWARD);
-        wheelFR.setDirection(DcMotor.Direction.FORWARD);
-        wheelBR.setPower(0);
-        wheelBL.setPower(0);
-        wheelFL.setPower(0);
-        wheelFR.setPower(0);
+        BR.setDirection(DcMotor.Direction.FORWARD);
+        BL.setDirection(DcMotor.Direction.FORWARD);
+        FL.setDirection(DcMotor.Direction.FORWARD);
+        FR.setDirection(DcMotor.Direction.FORWARD);
+        BR.setPower(0);
+        BL.setPower(0);
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
     }
 
     public void loop(){
 
-        double forward = gamepad1.right_stick_y;
-        double rotate  = gamepad1.left_stick_x;
+        // DRIVE
+        double max = 1.0;
+        double forward = -gamepad1.left_stick_y;
+        double rotate = gamepad1.right_stick_x;
 
-        
+        List l = new ArrayList<>();
+        l.add(Math.abs(forward + rotate));
+        l.add(Math.abs(forward - rotate));
+        if ((double) Collections.max(l) > max) {
+            max = (double) Collections.max(l);
+        }
+
+        if (gamepad1.dpad_up) {
+            setDrive(1, 1, 1, 1);
+        } else if (gamepad1.dpad_down) {
+            setDrive(-1, -1, -1, -1);
+        } else if (gamepad1.dpad_left) {
+            setDrive(-1, 1, 1, -1);
+        } else if (gamepad1.dpad_right) {
+            setDrive(1, -1, -1, 1);
+        } else {
+            FL.setPower((forward + rotate) / max);
+            BL.setPower((forward + rotate) / max);
+            FR.setPower((forward - rotate) / max);
+            BR.setPower((forward - rotate) / max);
+
+        }
 
 
+
+        // INDEXER
+        if (gamepad2.left_bumper){
+            relicHand.setPosition(.5);
+        } else {
+            relicHand.setPosition(.12);
+        }
 
 
     }
 
-
+    protected void setDrive(double p1, double p2, double p3, double p4) {
+        FL.setPower(p1);
+        FR.setPower(p2);
+        BL.setPower(p3);
+        BR.setPower(p4);
+    }
 
 }
