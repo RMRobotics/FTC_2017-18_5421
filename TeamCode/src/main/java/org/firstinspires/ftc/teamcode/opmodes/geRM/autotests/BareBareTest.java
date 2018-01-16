6e1e33b8121fab521c319fde83107cd0467e9cf7
Simon Liu <michaelblob99@gmail.com>
2018-01-16 16:59:06 -0500

    bare bare test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/geRM/autotests/BareBareTest.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode.opmodes.geRM.autotests;
+
+import com.kauailabs.navx.ftc.AHRS;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.util.enums.Direction;
+
+/**
+ * Created by michaelblob on 1/16/18.
+ */
+
+@Autonomous(name = "barebaretest")
+public class BareBareTest extends LinearOpMode {
+
+    protected ElapsedTime runtime = new ElapsedTime();
+
+    protected DcMotor FL;
+    protected DcMotor FR;
+    protected DcMotor BL;
+    protected DcMotor BR;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // motor initialization
+        FL = hardwareMap.dcMotor.get("FL");
+        FR = hardwareMap.dcMotor.get("FR");
+        BL = hardwareMap.dcMotor.get("BL");
+        BR = hardwareMap.dcMotor.get("BR");
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        BL.setDirection(DcMotor.Direction.REVERSE);
+        setZeroMode(DcMotor.ZeroPowerBehavior.BRAKE);
+        setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        // DRIVE FORWARD A LITTLE
+        setDrive(1.0);
+        sleep(2000);
+        setDrive(0.0);
+        sleep(2000);
+        setDrive(-1.0);
+        sleep(2000);
+        stop();
+    }
+
+    protected void setMode(DcMotor.RunMode r) {
+        FL.setMode(r);
+        FR.setMode(r);
+        BL.setMode(r);
+        BR.setMode(r);
+    }
+
+    protected void setZeroMode(DcMotor.ZeroPowerBehavior z) {
+        FL.setZeroPowerBehavior(z);
+        FR.setZeroPowerBehavior(z);
+        BL.setZeroPowerBehavior(z);
+        BR.setZeroPowerBehavior(z);
+    }
+
+    protected void setDrive(double p) {
+        FL.setPower(p);
+        FR.setPower(p);
+        BL.setPower(p);
+        BR.setPower(p);
+    }
+
+}
