c0e6b9bc80d5bc51c4a7363f405984c6669b4a6d
DanWilliams24 <01danielwilliams@gmail.com>
2018-01-30 15:43:46 -0500

    Created State Interface

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/geRM/autotests/stateMachine/State.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes.geRM.autotests.stateMachine;
+
+/**
+ * Created by Daniel on 1/30/18.
+ */
+
+public interface State {
+    void pull(AutoPullChain wrapper);
+}
+
