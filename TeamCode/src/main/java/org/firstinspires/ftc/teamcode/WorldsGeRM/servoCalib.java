e68a426c3cf9991b3a535815cb9b30464a7c4991
Angela Mu <angelamu2012@gmail.com>
2018-03-15 16:47:54 -0400

    edited servo calibration, created dumpBotTele and dumpBotTeleSuper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsGeRM/servoCalib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsGeRM/servoCalib.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.core.TeleSuper;
  * Created by Angela on 3/13/2018.
  */
 
-public class servoCalib {
+public class servoCalib extends dumpBotTele{
 
     /**
      * Created by Angela on 2/15/2018.
@@ -20,57 +20,45 @@ public class servoCalib {
         @Override
         public void loop() {
             if(gamepad2.x) {
-                relicArm.setPower(.85);
+                relicArm.setPosition(relicArm.getPosition()+.01);
             }
             else if(gamepad2.y) {
-                relicArm.setPower(-.85);
-            } else{
-                relicArm.setPower(0);
+                relicArm.setPosition(relicArm.getPosition()-.01);
             }
 
             if(gamepad2.a) {
-                relicClaw.setPower(.85);
+                relicClaw.setPosition(relicClaw.getPosition() + .01);
             }
             else if(gamepad2.b) {
-                relicClaw.setPower(-.85);
-            } else{
-                relicClaw.setPower(0);
+                relicClaw.setPosition(relicClaw.getPosition()-.01);
             }
 
             if(gamepad2.dpad_up) {
-                flipForw.setPower(.85);
+                flipForw.setPosition(flipForw.getPosition() + .01);
             }
             else if(gamepad2.dpad_down) {
-                flipForw.setPower(-.85);
-            } else{
-                flipForw.setPower(0);
+                flipForw.setPosition(flipForw.getPosition() - .01);
             }
 
             if(gamepad2.dpad_left) {
-                flipBack.setPower(.85);
+                flipBack.setPosition(flipBack.getPosition() + .01);
             }
             else if(gamepad2.dpad_right) {
-                flipBack.setPower(-.85);
-            } else{
-                flipBack.setPower(0);
+                flipBack.setPosition(flipBack.getPosition() - .01);
             }
 
             if(gamepad2.left_bumper) {
-                gemBarWrist.setPower(.85);
+                gemBarWrist.setPosition(gemBarWrist.getPosition() + .01);
             }
             else if(gamepad2.right_bumper) {
-                gemBarWrist.setPower(-.85);
-            } else{
-                gemBarWrist.setPower(0);
+                gemBarWrist.setPosition(gemBarWrist.getPosition() - .01);
             }
 
             if(gamepad2.left_trigger > 0) {
-                gemBarShoulder.setPower(.85);
+                gemBarShoulder.setPosition(gemBarShoulder.getPosition() + .01);
             }
             else if(gamepad2.right_trigger > 0) {
-                gemBarShoulder.setPower(-.85);
-            } else{
-                gemBarShoulder.setPower(0);
+                gemBarShoulder.setPosition(gemBarShoulder.getPosition() - .01);
             }
         }
     }
