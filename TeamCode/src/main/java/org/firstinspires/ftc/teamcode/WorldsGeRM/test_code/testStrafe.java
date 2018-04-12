a7ed0a4196702ccc4a3c37403f3326a13ec70e79
Angela Mu <angelamu2012@gmail.com>
2018-04-12 16:41:28 -0400

    created wiggle time method and changed strafe method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsGeRM/test_code/testStrafe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsGeRM/test_code/testStrafe.java
@@ -9,18 +9,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.WorldsGeRM.dumpBotAutoSuper;
 
-@Autonomous(name="testEncoderStrafe", group="twoMotor")
+@Autonomous(name = "testEncoderStrafe", group = "twoMotor")
 public class testStrafe extends LinearOpMode {
 
-    static double CPI = (1120.0 * 0.66666)/(4.0 * Math.PI);
+    static double CPI = (1120.0 * 0.66666) / (4.0 * Math.PI);
 
     public DcMotor FL;
     public DcMotor FR;
     public DcMotor BL;
     public DcMotor BR;
 
-    public void runOpMode(){
-
+    public void runOpMode() {
         FL = hardwareMap.dcMotor.get("FL");
         FR = hardwareMap.dcMotor.get("FR");
         BL = hardwareMap.dcMotor.get("BL");
@@ -32,73 +31,73 @@ public class testStrafe extends LinearOpMode {
         BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-//        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-//        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-//        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-//        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         waitForStart();
 
-
-        strafeEncoders(24, 1);
-        holdUp(5);
-//        moveEncoders(6, 1);
-//        holdUp(5);
-//        moveEncoders(12, 1);
-//        holdUp(5);
-
+        simpleStrafeEncoders(24, 1);
+        sleep(5000);
+        simpleStrafeEncoders(24, -1);
+        sleep(5000);
     }
 
-    protected void strafeEncoders(double distanceInches, int dir){
+    protected void strafeEncoders(double distanceInches, int dir) {
         //if rotate is one then the left drive train's target will be set to negative
-        double speed = 0.5;
+        double speed = 1;
         int currentPos = FL.getCurrentPosition();
-        int distanceTics = (int)(distanceInches * CPI);
+        int distanceTics = (int) (distanceInches * CPI);
         double tickRatio;
-
-        //BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
-        //BL.setTargetPosition(currentPos + distanceTics);
-        //FL.setTargetPosition(currentPos + distanceTics);
-        FL.setTargetPosition(currentPos + distanceTics);
-        //FR.setTargetPosition(currentPos + distanceTics);
-
-        //BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        while(FL.isBusy() /*&& BL.isBusy() && BR.isBusy() && FL.isBusy()*/){
-            tickRatio = ((double)FL.getCurrentPosition() - (double)currentPos) / distanceTics;
+        FL.setTargetPosition(currentPos + distanceTics);
+
+        while (FL.isBusy()) {
+            tickRatio = ((double) FL.getCurrentPosition() - (double) currentPos) / distanceTics;
             speed = ((-0.8) * (tickRatio) + 0.8);
-            if (speed < 0.4)
-                speed = 0.4;
+            if (speed < 0.6)
+                speed = 0.6;
             telemetry.addData("" + speed, "");
             telemetry.addData("tickRatio" + tickRatio, "GetPos" + FL.getCurrentPosition());
             telemetry.update();
 
-            FL.setPower(speed*dir);
-            FR.setPower(-speed*dir);
-            BL.setPower(-speed*dir);
-            BR.setPower(speed*dir);
+            FL.setPower(speed * dir);
+            FR.setPower(-speed * dir);
+            BL.setPower(-speed * dir);
+            BR.setPower(speed * dir);
         }
     }
 
-    public void holdUp(double time)
-    {
+    protected void simpleStrafeEncoders(double distanceInches, int dir) {
+        //if rotate is one then the left drive train's target will be set to negative
+        double speed = 1;
+        int currentPos = FL.getCurrentPosition();
+        int distanceTics = (int) (distanceInches * CPI);
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        FL.setTargetPosition(currentPos + distanceTics*dir);
+
+        while (FL.isBusy()) {
+            telemetry.addData("" + speed, "");
+            telemetry.update();
+
+            FL.setPower(speed * dir);
+            FR.setPower(-speed * dir);
+            BL.setPower(-speed * dir);
+            BR.setPower(speed * dir);
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+
+    public void holdUp(double time) {
         FR.setPower(0);
         BR.setPower(0);
         FL.setPower(0);
         BL.setPower(0);
         ElapsedTime timer = new ElapsedTime();
         timer.reset();
-        while (timer.seconds()<time) {
+        while (timer.seconds() < time) {
             telemetry.log().add("hold up");
             telemetry.clear();
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsGeRM/wiggleMethod.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsGeRM/wiggleMethod.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.WorldsGeRM;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -15,6 +16,8 @@ public class wiggleMethod extends LinearOpMode {
     public DcMotor FR;
     public DcMotor BL;
     public DcMotor BR;
+    protected BNO055IMU rev;
+    protected IIMU imu;
 
     public void runOpMode(){
 
@@ -25,10 +28,9 @@ public class wiggleMethod extends LinearOpMode {
         FL.setDirection(DcMotor.Direction.REVERSE);
         //FR.setDirection(DcMotor.Direction.REVERSE);
 
-
-        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 //        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
@@ -38,39 +40,33 @@ public class wiggleMethod extends LinearOpMode {
 
         waitForStart();
 
-        wiggle(8);
+        wiggleEncoder(8);
+        wiggleTime(4);
 
 
     }
 
-    protected void wiggle (int driveDistanceInches) {
+    protected void wiggleEncoder (int driveDistanceInches) {
         // travel 1 in every time while wiggling
-        double wiggleDistance = 1 * CPI;
+        double wiggleDistance = 5 * CPI;
         int wiggleCount = (int)(driveDistanceInches/wiggleDistance);
-        int distanceTics = (int) (driveDistanceInches * CPI);
         int count = 0;
 
-        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        for (int x = 1; x <= wiggleCount; x++)
+        for (int x = 0; x < wiggleCount; x++)
         {
             if (count == 0) {
-//                BL.setTargetPosition(BL.getCurrentPosition());
-//                FL.setTargetPosition(FL.getCurrentPosition());
-                BR.setTargetPosition((int)wiggleDistance);
-                FR.setTargetPosition((int)wiggleDistance);
+                FR.setTargetPosition(FR.getCurrentPosition() + (int) wiggleDistance);
             }
             else if (count == 1) {
-                BL.setTargetPosition((int)wiggleDistance);
-                FL.setTargetPosition((int)wiggleDistance);
-//                BR.setTargetPosition(BR.getCurrentPosition());
-//                FR.setTargetPosition(FR.getCurrentPosition());
+                FL.setTargetPosition(FL.getCurrentPosition() + (int)wiggleDistance);
             }
 
-            while (FR.isBusy() && BL.isBusy() && BR.isBusy() && FL.isBusy()) {
+            while (FR.isBusy() && FL.isBusy()) {
                 if (count == 0) {
                     FR.setPower(0.5);
                     BR.setPower(0.5);
@@ -79,12 +75,100 @@ public class wiggleMethod extends LinearOpMode {
                     count++;
                 } else if (count == 1) {
                     FL.setPower(0.5);
+                    BL.setPower(0.5);
                     BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                     FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                    BL.setPower(0.5);
                     count--;
                 }
             }
+            FR.setPower(0);
+            BR.setPower(0);
+            FL.setPower(0);
+            BL.setPower(0);
         }
+    }
+
+    protected void wiggleTime(int wiggleReps)
+    {
+        int count = 0;
+        for (int x = 0; x < wiggleReps; x++) {
+            if (x % 2 == 0) {
+                FR.setPower(0.5);
+                BR.setPower(0.5);
+                sleep(1000);
+                FR.setPower(0);
+                BR.setPower(0);
+            }
+            else {
+                FL.setPower(0.5);
+                BL.setPower(0.5);
+                sleep(1000);
+                FL.setPower(0);
+                BL.setPower(0);
+            }
+        }
+        imuTurn(0);
+    }
+
+    protected void imuTurn(double degree)
+    {
+        imu.setOffset(0);
+        double num = 0.75, err = 0.5, pwr = 0;
+
+        int count = 0;
+        boolean flag = true;
+        boolean dir_cw;
+
+        if (degree>0)
+        {
+            dir_cw = true;
+            FL.setPower(num);
+            BL.setPower(num);
+            FR.setPower(-1*num);
+            BR.setPower(-1*num);
+        }
+        else
+        {
+            dir_cw = false;
+            FL.setPower(-1*num);
+            BL.setPower(-1*num);
+            FR.setPower(num);
+            BR.setPower(num);
+        }
+
+        while (flag)
+        {
+            if (Math.abs(imu.getZAngle()-degree)<err) {
+                flag = false;
+                FL.setPower(0);
+                BL.setPower(0);
+                FR.setPower(0);
+                BR.setPower(0);
+            }
+            else if (dir_cw && imu.getZAngle()>degree)
+            {
+                FL.setPower(-1*pwr);
+                BL.setPower(-1*pwr);
+                FR.setPower(pwr);
+                BR.setPower(pwr);
+                count+=1;
+                dir_cw = !dir_cw;
+            }
+            else if (!dir_cw && imu.getZAngle()<degree)
+            {
+                FL.setPower(pwr);
+                BL.setPower(pwr);
+                FR.setPower(-1*pwr);
+                BR.setPower(-1*pwr);
+                count+=1;
+                dir_cw = !dir_cw;
+            }
+        }
+
+        FL.setPower(0);
+        BL.setPower(0);
+        FR.setPower(0);
+        BR.setPower(0);
+        telemetry.addData("power",pwr);
     }
 }
